2dd278b0bb46167ad3a4fbddfe8dee5f0afbb40c
Jeremy Trimble <devjeremyt@gmail.comgit config --global user.name Jeremy>
2019-10-24 22:47:53 -0400

    Setup basic layout. Created app.js and index.ejs

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/restful_blog_app",{ useNewUrlParser: true, useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", (req, res)=>{
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res)=>{
+    Blog.find({}, (err, blogs)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("Blog App Server listening on port 3000");
+});
