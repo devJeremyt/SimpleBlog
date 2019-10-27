4b14cd0930c6f807967512396c4c28465789345a
Jeremy Trimble <devjeremyt@gmail.com>
2019-10-27 19:59:48 -0400

    Added methodOverride. Added blog edit ing

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express'),
 app = express(),
 bodyParser = require('body-parser'),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+methodOverride = require('method-override');
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app",{ useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -53,8 +55,27 @@ app.get('/blogs/:id', (req,res)=>{
         } else{
             res.render('show', {blog:foundBlog});
         };
+    });    
+});
+
+app.get('/blogs/:id/edit', (req,res)=>{
+    Blog.findById(req.params.id, (err, foundBlog)=>{
+        if(err){
+            res.redirect('/blog');
+        } else{
+            res.render('edit', {blog: foundBlog});
+        };
+    })
+});
+
+app.put('/blogs/:id', (req,res)=>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
+        if(err){
+            res.redirect('/blogs');
+        } else{
+            res.redirect('/blogs/' + req.params.id);
+        };
     });
-    
 });
 
 app.listen(3000, ()=>{
