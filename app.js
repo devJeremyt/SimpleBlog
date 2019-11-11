a77b230275de355c2f1527418fe381f93767550b
Jeremy Trimble <devjeremyt@gmail.com>
2019-11-10 22:44:15 -0500

    Sanitized text fields, and styled index

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express'),
 app = express(),
 bodyParser = require('body-parser'),
 mongoose = require('mongoose'),
-methodOverride = require('method-override');
+methodOverride = require('method-override'),
+expressSanitizer = require('express-sanitizer');
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app",{ useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(expressSanitizer());
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -34,11 +36,14 @@ app.get('/blogs', (req, res)=>{
     });
 });
 
+//NEW ROUTE
 app.get('/blogs/new', (req,res)=>{
         res.render('new');
 });
 
+//CREATE ROUTE
 app.post('/blogs',(req,res)=>{
+    req.body.blog.body = req.sanitize(re.body.blog.body);
     Blog.create(req.body.blog, (err, newBlog)=>{
         if(err){
             console.log(err);
@@ -48,6 +53,7 @@ app.post('/blogs',(req,res)=>{
     });
 });
 
+//SHOW ROUTE
 app.get('/blogs/:id', (req,res)=>{
     Blog.findById(req.params.id, (err, foundBlog)=>{
         if(err){
@@ -58,6 +64,7 @@ app.get('/blogs/:id', (req,res)=>{
     });    
 });
 
+//EDIT ROUTE
 app.get('/blogs/:id/edit', (req,res)=>{
     Blog.findById(req.params.id, (err, foundBlog)=>{
         if(err){
@@ -68,7 +75,9 @@ app.get('/blogs/:id/edit', (req,res)=>{
     })
 });
 
+//UPDATE ROUTE
 app.put('/blogs/:id', (req,res)=>{
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
         if(err){
             res.redirect('/blogs');
@@ -78,6 +87,7 @@ app.put('/blogs/:id', (req,res)=>{
     });
 });
 
+//DELETE ROUTE
 app.delete('/blogs/:id', (req,res)=>{
     Blog.findByIdAndDelete(req.params.id, (err)=>{
         if(err){
